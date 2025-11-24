741add559b274955ddd7d7e9cc024929d3f5629a
nesrine <nesrine.fezzani@esprit.tn>
2025-11-24 16:24:42 +0100

    Keywords Bloc & Chambre

--- src/main/java/tn/esprit/tpfoyer/repositories/BlocRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repositories/BlocRepository.java
@@ -3,6 +3,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Bloc;
 
+import java.util.List;
+
 @Repository
 public interface BlocRepository extends JpaRepository<Bloc, Long> {
+    List<Bloc> findByFoyerIsNull();
+    List<Bloc> findByCapaciteGreaterThan(Integer capacite);
+    List<Bloc> findByNomStartingWith(String prefix);
+    List<Bloc> findByNomStartingWithAndCapaciteGreaterThan(String prefix, Integer capacite);
+
 }
--- src/main/java/tn/esprit/tpfoyer/repositories/ChambreRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repositories/ChambreRepository.java
@@ -3,6 +3,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Chambre;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ChambreRepository extends JpaRepository<Chambre, Long> {
+    List<Chambre> findByType(String type);
+    Optional<Chambre> findByNumero(String numero);
 }
